e9c95dd5837445a5036193005e4c7a6242844bb5
Mekhron Khusainov <mekhronkhusainov@uptimizator.com>
2020-04-07 13:04:55 -0400

    refactoring

--- saveResponse.js
+++ saveResponse.js
@@ -3,21 +3,21 @@ const fs = require('fs')
 
 
 let date = new Date()
-let timestamp = '';
+let timestamp = ''
 fetch('https://covidtracking.com/api/states')
   .then(response => response.json())
   .then(data => {
     timestamp = date.getTime()
     fs.open(`./savedResponses/${timestamp}.txt`, 'w', function (err, file) {
-        if (err) throw err;
-        console.log('Created file');
+        if (err) throw err
+        console.log('Created file')
       })
     return data
   })
   .then(data => {
     fs.writeFile(`./savedResponses/${timestamp}.txt`, JSON.stringify(data), function (err) {
-        if (err) throw err;
-        console.log('Saved to the file');
+        if (err) throw err
+        console.log('Saved to the file')
       })
   })
   .catch(err => console.log(err))
