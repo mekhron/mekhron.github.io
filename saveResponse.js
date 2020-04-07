9b8dd3dbf4b484dafb8c7a995c40434d2340e0bc
Mekhron Khusainov <mekhronkhusainov@uptimizator.com>
2020-04-07 12:59:13 -0400

    save responses to txt file

---
+++ saveResponse.js
@@ -0,0 +1,23 @@
+const fetch = require('node-fetch')
+const fs = require('fs')
+
+
+let date = new Date()
+let timestamp = '';
+fetch('https://covidtracking.com/api/states')
+  .then(response => response.json())
+  .then(data => {
+    timestamp = date.getTime()
+    fs.open(`./savedResponses/${timestamp}.txt`, 'w', function (err, file) {
+        if (err) throw err;
+        console.log('Created file');
+      })
+    return data
+  })
+  .then(data => {
+    fs.writeFile(`./savedResponses/${timestamp}.txt`, JSON.stringify(data), function (err) {
+        if (err) throw err;
+        console.log('Saved to the file');
+      })
+  })
+  .catch(err => console.log(err))
