6ea2c80f2f99022f5843a764e47d5fa24a5abf49
Mekhron Khusainov <mekhronkhusainov@uptimizator.com>
2020-04-14 00:15:14 -0400

    added self pinging

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
-// server.js
 const express = require('express');
+const fetch = require("node-fetch")
 
 // Define Express App
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const time = 1000*60*29
+const appName = 'http://localhost:'+PORT
+
+
 // Serve Static Assets
 app.use(express.static('public'));
+
+setInterval(() => {
+    fetch(appName)
+        .then(res => {
+            return res.status
+        })
+        .then(status => {
+            console.log(status)
+        })
+        .catch(res => {
+            console.log("Booo")
+        })
+}, time)
 
 app.listen(PORT, () => {
     console.log('Server connected at:', PORT);
